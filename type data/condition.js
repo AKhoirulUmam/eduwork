fc9995a4819135531af18bfa272656f36da85765
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-03-25 23:46:43 -0700

    Update condition.js

--- type data/condition.js
+++ type data/condition.js
@@ -1,5 +1,12 @@
 let x =20;
 let y=10;
 
-if(x>y)="hore saya bisa";
-console.log(x>y)="hore saya bisa";
+if (x>y){
+   alert="hore saya bisa";
+};
+console.log(x>y);
+
+else {
+    alert="saya tidak bisa"
+}
+console.log(x>y);
