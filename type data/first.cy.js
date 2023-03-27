39d7c4125abadea010a2126f07ce8b2da288f210
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-03-27 15:18:03 +0700

    commit pertama

--- type data/first.cy.js
+++ type data/first.cy.js
@@ -4,15 +4,15 @@ describe('my first test',()=>{
     it('clicking "type"shows the right headings',()=>{
         cy.visit('https://example.cypress.io')
 
-        cy.pause()
-
         cy.contains("type").click()
 
         //should be on a new URL which includes '/command/actions'
+        cy.url().should('include','actions')
 
-        //get an input, type into it and verify that the value has been updated
-        cy.get('action-email')
-          .type('fake@gmail.com')
-          .should('have.value','fake@gmail.com')
+         //get an input, type into it and verify that the value has been updated
+        cy.get('.action-email')
+        .type('fake@gmail.com')
+        .should('have.value','fake@gmail.com')
+
     })
 })
