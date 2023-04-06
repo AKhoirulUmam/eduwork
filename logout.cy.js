1245d0e39e288fb6f2b28e0590f132d3c32806e1
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-04-06 09:41:27 +0700

    commit logout

---
+++ logout.cy.js
@@ -0,0 +1,19 @@
+describe("logout", function(){
+
+    it("Berhasil logout dengan benar", function(){
+  
+      cy.visit("http://zero.webappsecurity.com/login.html");
+  
+      cy.get("#user_login").type("username");
+  
+      cy.get("#user_password").type("password");
+  
+      cy.get("input[name=submit]").click();
+
+      cy.contains('username').click();
+        
+      cy.get("#logout_link").click();
+      });
+    
+});
+    
