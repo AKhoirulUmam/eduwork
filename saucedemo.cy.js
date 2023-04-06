f1718ac4713c9c8fad56f3ec7652bf4fc003b288
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-04-06 08:51:17 +0700

    comit saucedemo

--- saucedemo.cy.js
+++ saucedemo.cy.js
@@ -1,1 +1,14 @@
+describe("Automation Test Login", function () {
+    it("Berhasil login menggunakan akun standard_user", function () {
+      cy.visit("https://www.saucedemo.com/");
+  
+      cy.get("#user-name").type("standard_user");
+      cy.get("#password").type("secret_sauce");
+  
+      cy.get("#login-button").click();
+  
+      cy.url().should("include", "inventory.html");
+
+    });
+    });
 
