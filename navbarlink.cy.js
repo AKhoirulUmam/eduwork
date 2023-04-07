f1659bbb2428d35dd4fff355a785d29c016b4c2f
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-04-07 09:53:27 +0700

    link

---
+++ navbarlink.cy.js
@@ -0,0 +1,27 @@
+describe('Navbar Test', function(){
+
+    it('should display online bankingcontent', function(){
+    
+       cy.visit('http://zero.webappsecurity.com/index.html')
+
+       cy.contains('Online Banking').click();
+
+       cy.url().should('include','online-banking.html');
+
+       cy.get('h1').should('be.visible');
+
+       cy.contains('Account Summary').click();
+
+       cy.get("#user_login").type("username");
+  
+       cy.get("#user_password").type("password");
+  
+       cy.get("input[name=submit]").click();
+
+       cy.contains('Savings').click();
+
+
+});
+});
+
+
