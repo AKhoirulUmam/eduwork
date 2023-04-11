70bd15d7ab9910f589a18ac34d431335c0c95049
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-04-10 20:11:03 -0700

    Update saucedemo.cy.js

--- saucedemo.cy.js
+++ saucedemo.cy.js
@@ -1,14 +1,26 @@
 describe("Automation Test Login", function () {
-    it("Berhasil login menggunakan akun standard_user", function () {
-      cy.visit("https://www.saucedemo.com/");
-  
-      cy.get("#user-name").type("standard_user");
-      cy.get("#password").type("secret_sauce");
-  
-      cy.get("#login-button").click();
-  
-      cy.url().should("include", "inventory.html");
 
-    });
-    });
+  it("Berhasil login menggunakan akun standard_user", function () {
+   
+    cy.visit("https://www.saucedemo.com/");
+
+    cy.get("#user-name").type("standard_user");
+    
+    cy.get("#password").type("secret_sauce");
+
+    cy.get("#login-button").click();
+
+    cy.url().should("include", "inventory.html");
+
+    cy.get("#add-to-cart-sauce-labs-bolt-t-shirt").click();
+
+    cy.get("#shopping_cart_container").click();
+
+    cy.get("#checkout").click();
+
+
+   
+   })
+   
+  });
 
