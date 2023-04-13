6d870a9b453f370d7989e90bfeefb1a6cb71bb19
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-04-13 21:10:14 +0700

    update

--- searchbox.cy.js
+++ searchbox.cy.js
@@ -1,5 +1,3 @@
-/// <reference types="cypress" />
-
 describe('Searchbox Test',()=>{
     before(()=>{
         cy.visit('http://zero.webappsecurity.com')
@@ -8,10 +6,14 @@ describe('Searchbox Test',()=>{
     it('Should type into searchbox and submit',()=>{
         cy.get('#searchTerm').type('Online Banking{enter}')
 
+        cy.get('h2').contains('Search Result')
+
         cy.contains('Online Banking').click();
 
         cy.get('#searchTerm').type('Online Statements{enter}')
 
+        cy.get('h2').contains('Search Result')
     });
 
 });
+
