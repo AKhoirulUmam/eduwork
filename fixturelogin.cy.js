6ecc5c1999ad65c61ebd06ae1cb73c80d60832c2
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-04-13 21:20:16 -0700

    Update fixturelogin.cy.js

--- fixturelogin.cy.js
+++ fixturelogin.cy.js
@@ -1,21 +1,15 @@
-describe('login with fixtures data',function(){
+describe("login with fixtures data",function(){
 
-    it('Should try to login',function(){
+    it('Should try to login',function() {
 
-            cy.visit("https://www.saucedemo.com/");
-
-            cy.fixture("user").then(user=>{
-
-            cy.get("#user-name").type(data.daftar_user.standard.username);
-    
-            cy.get("#password").type(data.daftar_user.standard.password);
-
-            cy.get("#login-button").click();
-
-            cy.url().should("include", "inventory.html");
-
-           
-
+        cy.visit("https://www.saucedemo.com/");
+        cy.fixture("user").then(data => {
+          cy.get("#user-name").type(data.daftar_user.standard.username);
+          cy.get("#password").type(data.daftar_user.standard.password);
+      
+          cy.get("#login-button").click();
+      
+          cy.url().should("include", "inventory.html");
         })
+      })
     })
-})
