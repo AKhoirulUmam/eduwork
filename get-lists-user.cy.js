4c3a659057195dcffba8c9bdcee767e654e07c1e
Ahmad Khoirul Umam <126857158+AKhoirulUmam@users.noreply.github.com>
2023-04-24 20:09:43 -0700

    Update get-lists-user.cy.js

--- get-lists-user.cy.js
+++ get-lists-user.cy.js
@@ -1,11 +1,10 @@
 it.only('Validate Header', () => {
-
+    
     cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-
+    
     cy.get('@pokemon').its('headers').its('content-type')
+    
+        .should('include', 'application/json; charset=utf-8')
 
-    .should('include', 'application/json; charset=utf-8')
+    cy.get('@pokemon').its('body').should('include',{name:"ditto"})
 });
-     '{"abilities":[{"ability":{"name":"limber","url":"https://pokeapi.co/api/v2/type/1/"}}],"weight":40}'
-
-    '{"abilities":[{"ability": {"name": "limber","url": "https://pokeapi.co/api/v2/ability/7/"}}]}'
