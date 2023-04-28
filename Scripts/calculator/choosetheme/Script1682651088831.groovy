c584344113b6177a36a95fa822743ec9faf8128d
Ahmad khoirul umam <umam0800@gmail.com>
2023-04-27 20:50:13 -0700

    first time

---
+++ Scripts/calculator/choosetheme/Script1682651088831.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\Acer\\Documents\\UDUAPP\\Calculator_v8.1 .apk', false)
+
+Mobile.tap(findTestObject('android.widget.Button - 7'), 0)
+
+Mobile.tap(findTestObject('android.widget.Button -x'), 0)
+
+Mobile.tap(findTestObject('android.widget.Button - 8'), 0)
+
+Mobile.tap(findTestObject('android.widget.Button --'), 0)
+
+Mobile.tap(findTestObject('android.widget.Button - AC'), 0)
+
+Mobile.closeApplication()
+
